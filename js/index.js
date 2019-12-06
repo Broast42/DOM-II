2656cf9b218e72c53c6b9d5e1c7881b301110fa8
rdowd <rjarrettdowd@yahoo.com>
2019-12-06 18:30:53 -0500

    Got 2 events done. links grow on hover and logo skews on page load.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+const allAs = document.querySelectorAll('a');
+allAs.forEach(i => {
+    i.addEventListener('mouseover', ()=>{
+       i.style.fontSize = "2rem"; 
+    });
+});
+
+allAs.forEach(i => {
+    i.addEventListener('mouseleave', ()=>{
+       i.style.fontSize = "1.6rem"; 
+    });
+});
+
+const logo = document.querySelector(".logo-heading");
+
+window.addEventListener("load", ()=>{
+    logo.style.transform = "skewX(-40deg)";
+    logo.style.transition = "transform 3s";
+    logo.style.transitionDelay = "2s";
+
+});
