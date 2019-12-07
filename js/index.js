6e41809e6ac9294792e02c3263d57808f5685fe2
rdowd <rjarrettdowd@yahoo.com>
2019-12-06 22:08:52 -0500

    Finished last of the events prevented default action on links and stoped propagation on conflicting events

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-//mouseover links to make them grow
+//mouseover links to make them grow and mouse out to make them go back
 const allAs = document.querySelectorAll('a');
 allAs.forEach(i => {
     i.addEventListener('mouseover', ()=>{
@@ -60,5 +60,78 @@ window.addEventListener("scroll", ()=>{
     lastScrollPos = currentScrollPos;
 });
 
+//on key press change section h2's to background black and color white
+const allH2 = document.querySelectorAll("h2");
+
+document.addEventListener("keydown", ()=>{
+    allH2.forEach(i =>{
+        i.style.background = "black";
+        i.style.color = "white";
+    });
+});
+
+//changes p text in destination section when you copy h2
+const lastH2 = document.querySelector(".content-destination h2");
+const destP = document.querySelector(".content-destination p");
+
+lastH2.addEventListener("copy", () =>{
+    destP.textContent = "You copied the text";
+});
+
+// change content pick section background color on resize
+const contentPick = document.querySelector(".content-pick");
+
+window.addEventListener("resize", () =>{
+    contentPick.style.background = "cyan";
+});
+
+//changes background to red whe you drag top img
+const topImg = document.querySelector(".intro img");
+const body = document.querySelector("body");
+
+topImg.addEventListener("drag", ()=>{
+    body.style.background = "red";
+});
+
+//added an imput so when you focus on it, it turns links red and select the text inside to turn them back to black
+const container = document.querySelector("div.container");
+const input = document.createElement("input");
+input.value = "select this text";
+container.append(input);
+
+input.addEventListener("focus", ()=>{
+    allAs.forEach(i => {
+        i.style.color = "red";
+    });
+});
+
+input.addEventListener("select", ()=>{
+    allAs.forEach(i => {
+        i.style.color = "black";
+    });
+});
+
+//prevent defaults on links
+
+allAs.forEach(i => {
+    i.addEventListener("click", (event) =>{
+    event.preventDefault();
+    });
+});
+
+//prevent propagation
+
+const destSection = document.querySelector(".content-destination");
+// const lastH2 = document.querySelector(".content-destination h2");
+// const destP = document.querySelector(".content-destination p");
+
+destP.addEventListener("click", (event)=>{
+    lastH2.style.color = "blue";
+    event.stopPropagation();
+});
+
+destSection.addEventListener("click", ()=>{
+    lastH2.style.color = "red";
+});
 
 
